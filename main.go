bec758fc1733bbf143ba8b3eba6195dac814d070
Jose Ignacio <jose.sperk@dreamlab.net>
2020-10-05 20:05:49 -0300

    Update

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"path/filepath"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -16,7 +18,7 @@ func main() {
 
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
@@ -34,11 +36,34 @@ func main() {
 		panic(err.Error())
 	}
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	nodos, _ := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 
-	api := clientset.CoreV1()
+	log.Println(nodos)
 
-	log.Print(api)
+	// for {
+	// 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// 	// Examples for error handling:
+	// 	// - Use helper functions like e.g. errors.IsNotFound()
+	// 	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	// 	namespace := "default"
+	// 	pod := "example-xxxxx"
+	// 	_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+	// 	if errors.IsNotFound(err) {
+	// 		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	// 	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	// 		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+	// 			pod, namespace, statusError.ErrStatus.Message)
+	// 	} else if err != nil {
+	// 		panic(err.Error())
+	// 	} else {
+	// 		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	// 	}
+
+	// 	time.Sleep(10 * time.Second)
+	// }
 }
